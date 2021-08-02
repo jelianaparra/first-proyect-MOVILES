44a582e27e5057c6402b0ed9e33a7f575147ccd0
jelianaparra <78937260+jelianaparra@users.noreply.github.com>
2021-08-01 23:40:40 -0400

    Update properties.js

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB: "mongodb://localhost:27017/proyecto-moviles",
+  DB: "mongodb+srv://jeliana:matias.2014@movies.napic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
